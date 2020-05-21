2efa79ebf52dfd65ae7bf2f12404f87d7af694f2
Ruslan <ruslanlogin78@gmail.com>
2020-05-21 16:33:51 +0300

    2ndndn

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int m, k, q, z;
+            Console.Write("Введіть число: ");
+            m = Convert.ToInt32(Console.ReadLine());
+            k = m % 10;
+            m = m / 10;
+            z = m % 10;
+            q = m / 10;
+            Console.WriteLine($"Цифра №1: {q}   ");
+            m %= 10;
+            Console.WriteLine($"Цифра №3: { k}  ");
+
+        }
+    }
+}
